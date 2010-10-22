84a9d6148821d5ebcf9eb413e57eadceed355e2d
moltengolem@hotmail.com <moltengolem@hotmail.com@f4664f4b-71e7-fb25-e4a8-2f905ea24f8f>
2010-10-22 13:04:39 +0000

    Update mouse Events

--- trunk/Programme/HoraireAlpha/HoraireAlpha/Form1.Designer.cs
+++ trunk/Programme/HoraireAlpha/HoraireAlpha/Form1.Designer.cs
@@ -31,29 +31,20 @@ namespace HoraireAlpha
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Horaire));
-            this.buttonHoraire = new System.Windows.Forms.Button();
             this.panelGauche = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.panelCentral = new System.Windows.Forms.Panel();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.userControl11 = new HoraireAlpha.UserControl1();
             this.panelGauche.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // buttonHoraire
-            // 
-            this.buttonHoraire.BackColor = System.Drawing.SystemColors.Control;
-            this.buttonHoraire.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonHoraire.BackgroundImage")));
-            this.buttonHoraire.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonHoraire.Location = new System.Drawing.Point(231, 0);
-            this.buttonHoraire.Name = "buttonHoraire";
-            this.buttonHoraire.Size = new System.Drawing.Size(120, 25);
-            this.buttonHoraire.TabIndex = 0;
-            this.buttonHoraire.Text = "Horaire";
-            this.buttonHoraire.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
-            this.buttonHoraire.UseVisualStyleBackColor = false;
-            this.buttonHoraire.Click += new System.EventHandler(this.buttonHoraire_Click);
             // 
             // panelGauche
             // 
@@ -63,31 +54,15 @@ namespace HoraireAlpha
             this.panelGauche.Size = new System.Drawing.Size(231, 545);
             this.panelGauche.TabIndex = 1;
             this.panelGauche.Paint += new System.Windows.Forms.PaintEventHandler(this.panelGauche_Paint);
+            this.panelGauche.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnMouseEvent);
+            this.panelGauche.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseEvent);
             // 
-            // button1
+            // monthCalendar1
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(346, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 25);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Profil";
-            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(468, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 25);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Paramètre";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.monthCalendar1.Location = new System.Drawing.Point(0, 51);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
             // 
             // button3
             // 
@@ -101,22 +76,79 @@ namespace HoraireAlpha
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // panelCentral
+            // tabControl1
             // 
-            this.panelCentral.Location = new System.Drawing.Point(230, 25);
-            this.panelCentral.Name = "panelCentral";
-            this.panelCentral.Size = new System.Drawing.Size(648, 520);
-            this.panelCentral.TabIndex = 5;
-            this.panelCentral.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCentral_Paint);
-            this.panelCentral.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnMouseEvent);
-            this.panelCentral.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseEvent);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.ItemSize = new System.Drawing.Size(58, 18);
+            this.tabControl1.Location = new System.Drawing.Point(229, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(649, 545);
+            this.tabControl1.TabIndex = 5;
             // 
-            // monthCalendar1 
+            // tabPage1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(0, 51);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
-            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.userControl11);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(641, 519);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(641, 519);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(641, 519);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(641, 519);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(641, 519);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(0, 0);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(387, 273);
+            this.userControl11.TabIndex = 0;
+            
             // 
             // Horaire
             // 
@@ -124,54 +156,48 @@ namespace HoraireAlpha
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(878, 545);
-            this.Controls.Add(this.panelCentral);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.panelGauche);
-            this.Controls.Add(this.buttonHoraire);
             this.Name = "Horaire";
             this.Text = "Horaire";
             this.panelGauche.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button buttonHoraire;
         private System.Windows.Forms.Panel panelGauche;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Panel panelCentral;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
 
         private void OnMouseEvent(object sender, MouseEventArgs e) 
             {
-                //Affiche les controles selon la couleur du BG (très fonctionnel)
-                if (this.panelCentral.BackColor == Color.Blue)
+            switch (e.Button)
                 {
-                    //Analyse les controles
-                    switch (e.Button)
-                    {
-                        case MouseButtons.Left:
-                            MessageBox.Show(this, "Vous avez appuyez sur gauche en : "+e.X+" , "+e.Y);
-                            break;
+                
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+                    break;
 
-                        case MouseButtons.Right:
-                            MessageBox.Show(this, "Vous avez appuyez sur droite en : "+e.X+" , "+e.Y);
-                            break;
-
-                    }
-                    //Analyse des X
-                    if (e.X > 400)
-                        MessageBox.Show(this, "Tu dépasse les 400 pixels en X");
-
-                    //Le handler fonctionne en boucle
+                case MouseButtons.Right:
+                    MessageBox.Show(this, "Vous avez appuyez sur droite en : " + e.X + " , " + e.Y);
+                    break;
 
                 }
+             
             }
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private UserControl1 userControl11;
         
         }
 }
---
+++ trunk/Programme/HoraireAlpha/HoraireAlpha/UserControl1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HoraireAlpha
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bouton fonctionnel");
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}

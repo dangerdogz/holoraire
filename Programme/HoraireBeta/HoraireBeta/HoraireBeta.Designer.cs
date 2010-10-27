8cbce5bb24e1eec4733a6c78e177224845e58eb4
moltengolem <moltengolem@hotmail.com>
2010-10-27 13:50:56 +0000

    Modification des noms de variables

--- Programme/HoraireBeta/HoraireBeta/HoraireBeta.Designer.cs
+++ Programme/HoraireBeta/HoraireBeta/HoraireBeta.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace HoraireBeta
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace HoraireBeta
 {
     partial class HoraireBeta
     {
@@ -30,26 +33,18 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.horaire = new System.Windows.Forms.TabPage();
+            this.panelCentral_Horaire = new System.Windows.Forms.Panel();
+            this.panelGauche_Horaire = new System.Windows.Forms.Panel();
             this.employe = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.tabPage4 = new System.Windows.Forms.TabPage();
-            this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.panel9 = new System.Windows.Forms.Panel();
-            this.panel10 = new System.Windows.Forms.Panel();
+            this.panelCentral_Employe = new System.Windows.Forms.Panel();
+            this.panelGauche_Employe = new System.Windows.Forms.Panel();
+            this.parametre = new System.Windows.Forms.TabPage();
+            this.panelCentral_Parametre = new System.Windows.Forms.Panel();
+            this.panelGauche_Parametre = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
             this.horaire.SuspendLayout();
             this.employe.SuspendLayout();
-            this.tabPage3.SuspendLayout();
-            this.tabPage4.SuspendLayout();
-            this.tabPage5.SuspendLayout();
+            this.parametre.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -57,9 +52,7 @@
             this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.Buttons;
             this.tabControl1.Controls.Add(this.horaire);
             this.tabControl1.Controls.Add(this.employe);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.parametre);
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -70,8 +63,8 @@
             // horaire
             // 
             this.horaire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.horaire.Controls.Add(this.panel2);
-            this.horaire.Controls.Add(this.panel1);
+            this.horaire.Controls.Add(this.panelCentral_Horaire);
+            this.horaire.Controls.Add(this.panelGauche_Horaire);
             this.horaire.Location = new System.Drawing.Point(4, 25);
             this.horaire.Name = "horaire";
             this.horaire.Padding = new System.Windows.Forms.Padding(3);
@@ -80,11 +73,30 @@
             this.horaire.Text = "Horaire";
             this.horaire.Click += new System.EventHandler(this.horaire_Click);
             // 
+            // panelCentral_Horaire
+            // 
+            this.panelCentral_Horaire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelCentral_Horaire.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCentral_Horaire.Location = new System.Drawing.Point(203, 7);
+            this.panelCentral_Horaire.Name = "panelCentral_Horaire";
+            this.panelCentral_Horaire.Size = new System.Drawing.Size(665, 515);
+            this.panelCentral_Horaire.TabIndex = 1;
+            // 
+            // panelGauche_Horaire
+            // 
+            this.panelGauche_Horaire.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelGauche_Horaire.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGauche_Horaire.Location = new System.Drawing.Point(7, 7);
+            this.panelGauche_Horaire.Name = "panelGauche_Horaire";
+            this.panelGauche_Horaire.Size = new System.Drawing.Size(189, 515);
+            this.panelGauche_Horaire.TabIndex = 0;
+            this.panelGauche_Horaire.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
             // employe
             // 
             this.employe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.employe.Controls.Add(this.panel3);
-            this.employe.Controls.Add(this.panel4);
+            this.employe.Controls.Add(this.panelCentral_Employe);
+            this.employe.Controls.Add(this.panelGauche_Employe);
             this.employe.Location = new System.Drawing.Point(4, 25);
             this.employe.Name = "employe";
             this.employe.Padding = new System.Windows.Forms.Padding(3);
@@ -93,132 +105,57 @@
             this.employe.Text = "Employé";
             this.employe.Click += new System.EventHandler(this.employe_Click);
             // 
-            // tabPage3
+            // panelCentral_Employe
             // 
-            this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.tabPage3.Controls.Add(this.panel5);
-            this.tabPage3.Controls.Add(this.panel6);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(877, 532);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.panelCentral_Employe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelCentral_Employe.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCentral_Employe.Location = new System.Drawing.Point(204, 8);
+            this.panelCentral_Employe.Name = "panelCentral_Employe";
+            this.panelCentral_Employe.Size = new System.Drawing.Size(664, 514);
+            this.panelCentral_Employe.TabIndex = 3;
+            this.panelCentral_Employe.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+          //  this.panelCentral_Employe.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MouseEvent);
             // 
-            // tabPage4
+            // panelGauche_Employe
             // 
-            this.tabPage4.BackColor = System.Drawing.Color.Silver;
-            this.tabPage4.Controls.Add(this.panel7);
-            this.tabPage4.Controls.Add(this.panel8);
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
-            this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(877, 532);
-            this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "tabPage4";
+            this.panelGauche_Employe.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelGauche_Employe.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGauche_Employe.Location = new System.Drawing.Point(8, 8);
+            this.panelGauche_Employe.Name = "panelGauche_Employe";
+            this.panelGauche_Employe.Size = new System.Drawing.Size(189, 514);
+            this.panelGauche_Employe.TabIndex = 2;
+            this.panelGauche_Employe.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
-            // tabPage5
+            // parametre
             // 
-            this.tabPage5.BackColor = System.Drawing.Color.Yellow;
-            this.tabPage5.Controls.Add(this.panel9);
-            this.tabPage5.Controls.Add(this.panel10);
-            this.tabPage5.Location = new System.Drawing.Point(4, 25);
-            this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(877, 532);
-            this.tabPage5.TabIndex = 4;
-            this.tabPage5.Text = "tabPage5";
+            this.parametre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.parametre.Controls.Add(this.panelCentral_Parametre);
+            this.parametre.Controls.Add(this.panelGauche_Parametre);
+            this.parametre.Location = new System.Drawing.Point(4, 25);
+            this.parametre.Name = "parametre";
+            this.parametre.Padding = new System.Windows.Forms.Padding(3);
+            this.parametre.Size = new System.Drawing.Size(877, 532);
+            this.parametre.TabIndex = 2;
+            this.parametre.Text = "Paramètre";
             // 
-            // panel1
+            // panelCentral_Parametre
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(7, 7);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(189, 515);
-            this.panel1.TabIndex = 0;
+            this.panelCentral_Parametre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelCentral_Parametre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelCentral_Parametre.Location = new System.Drawing.Point(204, 8);
+            this.panelCentral_Parametre.Name = "panelCentral_Parametre";
+            this.panelCentral_Parametre.Size = new System.Drawing.Size(664, 514);
+            this.panelCentral_Parametre.TabIndex = 3;
+            this.panelCentral_Parametre.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
             // 
-            // panel2
+            // panelGauche_Parametre
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Location = new System.Drawing.Point(203, 7);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(665, 515);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Location = new System.Drawing.Point(204, 8);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(664, 514);
-            this.panel3.TabIndex = 3;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel4.Location = new System.Drawing.Point(8, 8);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(189, 514);
-            this.panel4.TabIndex = 2;
-            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel5.Location = new System.Drawing.Point(204, 8);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(664, 514);
-            this.panel5.TabIndex = 3;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel6.Location = new System.Drawing.Point(8, 8);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(189, 514);
-            this.panel6.TabIndex = 2;
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel7.Location = new System.Drawing.Point(204, 8);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(664, 514);
-            this.panel7.TabIndex = 3;
-            // 
-            // panel8
-            // 
-            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel8.Location = new System.Drawing.Point(8, 8);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(189, 514);
-            this.panel8.TabIndex = 2;
-            // 
-            // panel9
-            // 
-            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel9.Location = new System.Drawing.Point(204, 8);
-            this.panel9.Name = "panel9";
-            this.panel9.Size = new System.Drawing.Size(664, 514);
-            this.panel9.TabIndex = 3;
-            // 
-            // panel10
-            // 
-            this.panel10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.panel10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel10.Location = new System.Drawing.Point(8, 8);
-            this.panel10.Name = "panel10";
-            this.panel10.Size = new System.Drawing.Size(189, 514);
-            this.panel10.TabIndex = 2;
+            this.panelGauche_Parametre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panelGauche_Parametre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelGauche_Parametre.Location = new System.Drawing.Point(8, 8);
+            this.panelGauche_Parametre.Name = "panelGauche_Parametre";
+            this.panelGauche_Parametre.Size = new System.Drawing.Size(189, 514);
+            this.panelGauche_Parametre.TabIndex = 2;
             // 
             // HoraireBeta
             // 
@@ -231,9 +168,7 @@
             this.tabControl1.ResumeLayout(false);
             this.horaire.ResumeLayout(false);
             this.employe.ResumeLayout(false);
-            this.tabPage3.ResumeLayout(false);
-            this.tabPage4.ResumeLayout(false);
-            this.tabPage5.ResumeLayout(false);
+            this.parametre.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -243,19 +178,13 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage horaire;
         private System.Windows.Forms.TabPage employe;
-        private System.Windows.Forms.TabPage tabPage3;
-        private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.TabPage tabPage5;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Panel panel9;
-        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.TabPage parametre;
+        private System.Windows.Forms.Panel panelCentral_Horaire;
+        private System.Windows.Forms.Panel panelGauche_Horaire;
+        private System.Windows.Forms.Panel panelCentral_Employe;
+        private System.Windows.Forms.Panel panelGauche_Employe;
+        private System.Windows.Forms.Panel panelCentral_Parametre;
+        private System.Windows.Forms.Panel panelGauche_Parametre;
     }
 }
 

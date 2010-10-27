8f169681faf0d65ea62094396e9cfbee9990877b
moltengolem <moltengolem@hotmail.com>
2010-10-27 14:31:32 +0000

    Mouse Events ajouté à Horaire,Parametre,Employé
    Si vous ajoutez des panels customs, copier le pattern en modifiant le nom
    du mouse event en conséquent

--- Programme/HoraireBeta/HoraireBeta/HoraireBeta.cs
+++ Programme/HoraireBeta/HoraireBeta/HoraireBeta.cs
@@ -11,6 +11,7 @@ namespace HoraireBeta
 {
     public partial class HoraireBeta : Form
     {
+       
         public HoraireBeta()
         {
             InitializeComponent();
@@ -18,17 +19,111 @@ namespace HoraireBeta
 
         private void horaire_Click(object sender, EventArgs e)
         {
+           
 
         }
 
         private void panel4_Paint(object sender, PaintEventArgs e)
         {
-
+          
         }
 
         private void employe_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pGauche_Employe_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+          
+           switch(e.Button)
+                {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelGauche Employé: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+                    
+                    break;
+                }
+        }
+
+        private void pCentral_Employe_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelCentral Employé: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+
+                    break;
+            }
+        }
+
+        private void pGauche_Horaire_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelGauche Horaire: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+
+                    break;
+            }
+        }
+
+        private void pCentral_Horaire_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelCentral Horaire: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+
+                    break;
+            }
+        }
+
+
+        private void pGauche_Parametre_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelGauche Parametre: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+
+                    break;
+            }
+        }
+
+        private void pCentral_Parametre_OnMouseEvent(object sender, MouseEventArgs e)
+        {
+
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    MessageBox.Show(this, "PanelCentral Parametre: Vous avez appuyez sur gauche en : " + e.X + " , " + e.Y);
+
+                    break;
+            }
         }
     }
 }
